8e63b3e8c427cff4f788c31f14bd1aac63591419
alinamozgunova16 <146935107+alinamozgunova16@users.noreply.github.com>
2025-12-02 16:31:32 +0300

    ;

---
+++ diplom_1/Migrations/20251202133032_AddOrganizationToComputerLicense1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace diplom_1.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrganizationToComputerLicense1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Editions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    ProductId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Editions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Editions_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Licenses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Article = table.Column<string>(type: "text", nullable: true),
+                    PaymentPeriod = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    ProductId = table.Column<int>(type: "integer", nullable: true),
+                    EditionId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Licenses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Licenses_Editions_EditionId",
+                        column: x => x.EditionId,
+                        principalTable: "Editions",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Licenses_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Computers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    BranchId = table.Column<int>(type: "integer", nullable: true),
+                    LicenseId = table.Column<int>(type: "integer", nullable: true),
+                    OrganizationId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Computers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Computers_Branches_BranchId",
+                        column: x => x.BranchId,
+                        principalTable: "Branches",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Computers_Licenses_LicenseId",
+                        column: x => x.LicenseId,
+                        principalTable: "Licenses",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Computers_Organizations_OrganizationId",
+                        column: x => x.OrganizationId,
+                        principalTable: "Organizations",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Modules",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Article = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    PaymentPeriod = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    ProductId = table.Column<int>(type: "integer", nullable: true),
+                    LicenseId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Modules", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Modules_Licenses_LicenseId",
+                        column: x => x.LicenseId,
+                        principalTable: "Licenses",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Modules_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ComputerLicenses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ComputerName = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    LicenseId = table.Column<int>(type: "integer", nullable: false),
+                    ComputerId = table.Column<int>(type: "integer", nullable: false),
+                    BranchId = table.Column<int>(type: "integer", nullable: true),
+                    ProductId = table.Column<int>(type: "integer", nullable: true),
+                    EditionId = table.Column<int>(type: "integer", nullable: true),
+                    ModuleId = table.Column<int>(type: "integer", nullable: true),
+                    OrganizationId = table.Column<int>(type: "integer", nullable: true),
+                    StartDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    Status = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ComputerLicenses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ComputerLicenses_Branches_BranchId",
+                        column: x => x.BranchId,
+                        principalTable: "Branches",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_ComputerLicenses_Computers_ComputerId",
+                        column: x => x.ComputerId,
+                        principalTable: "Computers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ComputerLicenses_Editions_EditionId",
+                        column: x => x.EditionId,
+                        principalTable: "Editions",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_ComputerLicenses_Licenses_LicenseId",
+                        column: x => x.LicenseId,
+                        principalTable: "Licenses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ComputerLicenses_Modules_ModuleId",
+                        column: x => x.ModuleId,
+                        principalTable: "Modules",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_ComputerLicenses_Organizations_OrganizationId",
+                        column: x => x.OrganizationId,
+                        principalTable: "Organizations",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_ComputerLicenses_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ComputerLicenses_BranchId",
+                table: "ComputerLicenses",
+                column: "BranchId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ComputerLicenses_ComputerId",
+                table: "ComputerLicenses",
+                column: "ComputerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ComputerLicenses_EditionId",
+                table: "ComputerLicenses",
+                column: "EditionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ComputerLicenses_LicenseId",
+                table: "ComputerLicenses",
+                column: "LicenseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ComputerLicenses_ModuleId",
+                table: "ComputerLicenses",
+                column: "ModuleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ComputerLicenses_OrganizationId",
+                table: "ComputerLicenses",
+                column: "OrganizationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ComputerLicenses_ProductId",
+                table: "ComputerLicenses",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Computers_BranchId",
+                table: "Computers",
+                column: "BranchId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Computers_LicenseId",
+                table: "Computers",
+                column: "LicenseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Computers_OrganizationId",
+                table: "Computers",
+                column: "OrganizationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Editions_ProductId",
+                table: "Editions",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Licenses_EditionId",
+                table: "Licenses",
+                column: "EditionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Licenses_ProductId",
+                table: "Licenses",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Modules_LicenseId",
+                table: "Modules",
+                column: "LicenseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Modules_ProductId",
+                table: "Modules",
+                column: "ProductId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ComputerLicenses");
+
+            migrationBuilder.DropTable(
+                name: "Computers");
+
+            migrationBuilder.DropTable(
+                name: "Modules");
+
+            migrationBuilder.DropTable(
+                name: "Licenses");
+
+            migrationBuilder.DropTable(
+                name: "Editions");
+        }
+    }
+}
